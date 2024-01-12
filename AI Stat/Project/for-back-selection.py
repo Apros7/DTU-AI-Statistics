8537887702764bb388a6f447b0da47a09a60eb22
apros7 <lucas.vilsen@gmail.com>
2024-01-12 23:53:37 +0100

    ready for forward and backward selection

--- AI Stat/Project/for-back-selection.py
+++ AI Stat/Project/for-back-selection.py
@@ -1,3 +1,6 @@
 from automation import Tester
 
-tester = Tester(final_test = False)
+tester = Tester(final_test = False, _run_tests = False)
+data_x, data_y = tester.get_data()
+data_fold_indexes = tester.get_data_folds()
+
