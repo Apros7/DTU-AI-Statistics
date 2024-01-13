67668f337429ef678953138217df1e0d1dee4a40
apros7 <lucas.vilsen@gmail.com>
2024-01-13 10:42:38 +0100

    normalized input

--- AI Stat/Project/automation.py
+++ AI Stat/Project/automation.py
@@ -36,6 +36,16 @@ def frust_class(x):
     else:
         return 1
 
+def normalize_column(df, columns):
+    for column in columns:
+        data = df[column]
+        min_val = data.min()
+        max_val = data.max()
+        normalized_column = (data - min_val) / (max_val - min_val)
+        df[column] = normalized_column
+    print(df.head())
+    return df
+
 class Tester():
 
     def __init__(
@@ -64,13 +74,14 @@ class Tester():
     def get_data(self): return self.data_x, self.data_y
     def get_data_folds(self): return self.fold_combs # List of (train_indexes, test_indexes) or (train_indexes, val_indexes, test_indexes), then use data_x[train_indexes], so on
     def get_data_columns(self): return self.x_cols, self.y_cols
-    def _load_data(self): self.data = pd.read_csv(self.path_to_data); self.columns = list(self.data.columns); self._fix_data()#; self._categorize_y_column();
+    def _load_data(self): self.data = pd.read_csv(self.path_to_data); self.columns = list(self.data.columns); self._fix_data(); self._categorize_y_column(); self._normalize_x_columns()
+    def _normalize_x_columns(self): self.data = normalize_column(self.data, ["HR_Mean", "HR_Median", "HR_std", "HR_Min", "HR_Max", "HR_AUC"])
     def _categorize_y_column(self): self.data["HighlyFrustrated"] = self.data["Frustrated"].apply(frust_class)
     def _reset_index(self): self.data = self.data.reset_index(drop=True)
     def _fix_data(self): self.data = self.data.astype({"Frustrated": str}); self.data = self.data[self.data["Cohort"] == "D1_2"]; self._reset_index()
     def _set_data_x(self, x_columns): self.data_x = torch.tensor(self.data[x_columns].values.astype(np.float32()))
     def _set_data_y(self, y_column): self.data_y = torch.tensor(self.data[y_column].values.astype(np.float32))
-    def _set_data_props(self, x_cols, y_col): self._set_data_x(self.x_cols); self._set_data_y(self.y_col)
+    def _set_data_props(self): self._set_data_x(self.x_cols); self._set_data_y(self.y_col)
     def _unnest_lst(self, lst): return [item for sublist in lst for item in sublist]
     def _get_fold_combs_without_val(self): self.fold_combs = [(self._unnest_lst(self.data_folds[:i] + self.data_folds[i+1:]), self.data_folds[i]) for i in range(self.k)] # (train, test)
     def _get_fold_combs_with_val(self): self.fold_combs = [(self._unnest_lst(self.data_folds[:i] + self.data_folds[i+2:]), self.data_folds[i+1 if i+1 < self.k else 0], self.data_folds[i]) for i in range(self.k)] # (train, val, test)
--- AI Stat/Project/for-back-selection.py
+++ AI Stat/Project/for-back-selection.py
@@ -8,5 +8,22 @@ data_x, data_y = tester.get_data()
 x_columns, y_columns = tester.get_data_columns()
 data_fold_indexes = tester.get_data_folds()
 
-def run_forward_selection():
+class Selector():
+    def __init__(self, ml_function, data_x, data_y, x_columns, data_fold_indexes) -> None:
+        self.ml_func, self.data_x, self.data_y, self.x_cols, self.data_folds = ml_function, data_x, data_y, x_columns, data_fold_indexes
+
+    def _run_one_fold(self, x_train, y_train, x_test, y_test): return self.ml_func(x_train, y_train, x_test, y_test)
+
+    def _get_all_forward_combs(self, data_x_implemented, data_x_left, columns_let_to_choose_from):
+        
+
+
+    def _run_one_forward_selection(self):
+        
+            results_this_fold = []
+            fold_train_x, fold_train_y, fold_test_x, fold_test_y = self.data_x[fold_train_indexes], self.data_y[fold_train_indexes], self.data_x[fold_test_indexes], self.data_y[fold_test_indexes]
+
+
+    fold_train_x, fold_train_y, fold_test_x, fold_test_y = self.data_x[fold_train_indexes], self.data_y[fold_train_indexes], self.data_x[fold_test_indexes], self.data_y[fold_test_indexes]
+        
 
